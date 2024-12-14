c372e00fdfe731dc5fe65faaa5d1ca23ae1a9794
tg2648 <45052145+tg2648@users.noreply.github.com>
2024-12-14 20:39:03 +0000

    Add basic db operations

---
+++ internal/models/reminders.go
@@ -0,0 +1,104 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type Reminder struct {
+	ID          int
+	Title       string
+	DueAt       *time.Time
+	DismissedAt *sql.NullTime
+	CreatedAt   *time.Time
+}
+
+type ReminderModel struct {
+	DB *sql.DB
+}
+
+// Insert new reminder into the DB
+// Return the ID of the new reminder
+func (m *ReminderModel) Insert(title string, dueAt *time.Time) (int, error) {
+	stmt := `
+		INSERT INTO reminders (title, due_at)
+		VALUES(?, ?)
+	`
+	result, err := m.DB.Exec(stmt, title, dueAt)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
+}
+
+// Return a specific Reminder by ID
+func (m *ReminderModel) Get(id int) (*Reminder, error) {
+	stmt := `
+		SELECT id, title, due_at, dismissed_at, created_at
+		FROM reminders
+		WHERE id = ?
+	`
+
+	r := &Reminder{}
+	err := m.DB.
+		QueryRow(stmt, id).
+		Scan(&r.ID, &r.Title, &r.DueAt, &r.DismissedAt, &r.CreatedAt)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+
+	return r, nil
+}
+
+// Return reminders due on or before a certain date
+func (m *ReminderModel) GetDue(date *time.Time) ([]*Reminder, error) {
+	stmt := `
+		SELECT id, title, due_at, dismissed_at, created_at
+		FROM reminders
+		WHERE date(due_at) <= date(?)
+	`
+
+	rows, err := m.DB.Query(stmt, date)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	reminders := []*Reminder{}
+
+	for rows.Next() {
+		r := &Reminder{}
+
+		err = rows.Scan(&r.ID, &r.Title, &r.DueAt, &r.DismissedAt, &r.CreatedAt)
+		if err != nil {
+			return nil, err
+		}
+
+		reminders = append(reminders, r)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return reminders, nil
+}
+
+// Return reminders due today
+func (m *ReminderModel) GetDueToday() ([]*Reminder, error) {
+	now := time.Now().UTC()
+	return m.GetDue(&now)
+}
--- main.go
+++ main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"sort"
+	"time"
 
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/tg2648/grem/internal/models"
 	"github.com/urfave/cli/v2"
 )
 
 const configDirName = ".grem"
+const dueDateLayout = "2006-01-02"
 
 func directoryExists(path string) bool {
 	info, err := os.Stat(path)
@@ -22,23 +28,69 @@ func directoryExists(path string) bool {
 	return info.IsDir()
 }
 
-func setup() {
-	var homeDir, err = os.UserHomeDir()
+func setup(db *sql.DB) {
+	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Fatalf("Unable to determine user's home directory: %s", err.Error())
 	}
 
-	var configDir = homeDir + "/" + configDirName
+	configDir := homeDir + "/" + configDirName
 	if !directoryExists(configDir) {
 		if err = os.Mkdir(configDir, 0755); err != nil {
 			log.Fatalf("Unable to create the config directory: %s", err.Error())
 		}
 		log.Printf("Created config directory in %s", configDir)
 	}
+
+	sqlStmt := `
+	CREATE TABLE IF NOT EXISTS reminders (
+		id INTEGER NOT NULL PRIMARY KEY,
+		title TEXT NOT NULL,
+		due_at DATE NOT NULL,
+		dismissed_at DATETIME DEFAULT NULL,
+		created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+	);
+	`
+
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
 }
 
+// The openDb() function wraps [sql.Open] and returns a sql.DB connection pool
+// for a given DSN.
+func openDb(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+// type application struct {
+// 	errorLog *log.Logger
+// 	reminders *models.ReminderModel
+// 	cliApp *cli.App
+// }
+
 func main() {
-	setup()
+	dsn := "file:grem.db"
+	db, err := openDb(dsn)
+
+	if err != nil {
+		log.Fatal("db error: ", err.Error())
+	}
+	defer db.Close()
+
+	setup(db)
+	reminders := &models.ReminderModel{DB: db}
 
 	app := &cli.App{
 		Name:            "grem",
@@ -56,14 +108,40 @@ func main() {
 						Usage: "Schedule a new reminder",
 						Flags: []cli.Flag{
 							&cli.StringFlag{
-								Name:     "title",
-								Aliases:  []string{"t"},
-								Usage:    "Reminder's `title`",
-								Required: true,
+								Name:    "title",
+								Aliases: []string{"t"},
+								Usage:   "Reminder's `title`",
+							},
+							&cli.TimestampFlag{
+								Name:    "due",
+								Aliases: []string{"d"},
+								Usage:   "Reminder's due `date` in the YYYY-MM-DD format",
+								Layout:  dueDateLayout,
 							},
 						},
 						Action: func(ctx *cli.Context) error {
-							fmt.Println("Adding reminder", ctx.String("title"))
+							title := ctx.String("title")
+							due := ctx.Timestamp("due")
+
+							fmt.Printf("title: %q\n", title)
+							fmt.Printf("due: %q\n", due)
+
+							// TODO: if no flags provided, show a TUI interface
+							if title == "" || due == nil {
+								return cli.Exit("Error: title and date are required", 1)
+							}
+
+							fmt.Println("Adding reminder")
+							id, err := reminders.Insert(title, due)
+							if err != nil {
+								return err
+							}
+							fmt.Println("Reminder added: ", id)
+
+							return nil
+						},
+						OnUsageError: func(ctx *cli.Context, err error, isSubcommand bool) error {
+							fmt.Println("Usage error: ", err.Error())
 							return nil
 						},
 					},
@@ -79,7 +157,29 @@ func main() {
 						ctx.App.Name,
 					), 1)
 			}
-			fmt.Println("Notifications:")
+
+			r, err := reminders.Get(4)
+			if err != nil {
+				if errors.Is(err, models.ErrNoRecord) {
+					log.Println("Reminder not found")
+				} else {
+					log.Println("Error getting reminder: ", err.Error())
+				}
+			} else {
+				log.Printf("Reminder (%d) %q due at %q\n", r.ID, r.Title, r.DueAt.Format("2006-01-02"))
+			}
+
+			fmt.Println("All Reminders:")
+			date, _ := time.Parse("2006-01-02", "2024-12-15")
+			due, err := reminders.GetDue(&date)
+			if err != nil {
+				return err
+			}
+
+			for _, reminder := range due {
+				fmt.Printf("%+v\n", reminder)
+			}
+
 			return nil
 		},
 	}
